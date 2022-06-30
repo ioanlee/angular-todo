745c056665d6302a1017edeac532903803962ebd
ioanlee <reddaravus@gmail.com>
2022-06-30 13:39:06 +1000

    all pages + spa done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule, Routes } from '@angular/router' 
 
-import { AppComponent } from './app.component';
+import { environment } from '../environments/environment'
+import { AppComponent } from './app.component'
+import { AppRoutingModule } from './app-routing.module'
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { TaskComponent } from './task/task.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditComponent } from './edit/edit.component'
+
+const routes:Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'task', component: TaskComponent },
+  { path: 'edit', component: EditComponent },
+  { path: '**', redirectTo: '/dashboard' },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    DashboardComponent,
+    EditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
